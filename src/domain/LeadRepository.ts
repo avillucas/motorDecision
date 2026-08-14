c9aaa9ffbe2a549ff2ad69d057e464ca0135ac05
Lucas Avila <avillucas@gmail.com>
2026-08-13 23:37:19 -0300

    reordenando todo el codigo

--- src/domain/LeadRepository.ts
+++
@@ -1,6 +0,0 @@
-import { LeadContacto, LeadListaEspera } from "./Lead";
-
-export interface LeadRepository {
-  saveContacto(sessionId: string, lead: LeadContacto): Promise<void>;
-  saveListaEspera(sessionId: string, lead: LeadListaEspera): Promise<void>;
-}
